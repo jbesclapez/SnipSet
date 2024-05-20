9dd49b30f5182c35995ec4922b9f6c80c64cb36b
Nicolas ARGENTE <nicolas_argente@hotmail.com>
2024-05-20 11:00:38 +0200

    Set up initial project structure with Tailwind CSS configuration

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+      <h1>Welcome to SnipSet</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Home'
+  };
+  </script>
+  
+  <style scoped>
+  /* Component-specific styles */
+  </style>
+  
